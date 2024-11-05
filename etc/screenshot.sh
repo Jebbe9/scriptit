8cf3ee059b48cc30c281d32e695578aedba32b09
Jebbe9 <jebbede@protonmail.com>
2024-11-05 21:22:18 +0200

    muutoksii

--- etc/screenshot.sh
+++
@@ -1,4 +0,0 @@
-fileName=$(date "+%Y-%m-%d_%H:%M:%S.png")
-maim -s /home/jere/Pictures/Screenshots/${fileName}
-xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/${fileName}
-notify-send -t 1500 -i ~/Pictures/Screenshots/${fileName} "Screenshot taken"
--- etc/srb2k.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export WINEPREFIX="/home/jere/Documents/srb2k/Bottle"
-
-cd /home/jere/Documents/srb2k && wine srb2kart-neptune.exe
